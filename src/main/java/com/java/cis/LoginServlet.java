d8f171f30318f8f370ca65145e7239cbf2c48726
asirichandana130699 <asirichandana130699@gmail.com>
2024-02-14 14:45:47 -0600

    Servlet

--- src/main/java/com/java/cis/LoginServlet.java
+++ src/main/java/com/java/cis/LoginServlet.java
@@ -10,7 +10,23 @@ import java.io.IOException;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
-
+    /***
+     * Do post method which takes request and response as the parameter which are called by the server.
+     * POST is used to send the information to the server.
+     * we took user name and password that are requested from web page as parameter. (this is linked by name attribute in static web page)
+     * now we perform some validation to check user name and password is matching or not.
+     * if matched{
+     *     we set the user name attribute with our username.
+     *     and we use request dispatcher for dispatching the request to our jsp web page.
+     *     we use forward method which sends the request from servlet to web page.
+     * }
+     * else{
+     *     we just redirect the index web page which is a unsuccessful.
+     * }
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     */
     protected void doPost(HttpServletRequest request,
                           HttpServletResponse response) throws ServletException, IOException {
         String userName = request.getParameter("username");
